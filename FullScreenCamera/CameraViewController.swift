8ca6c3c75f6401e4774fa9d0765dc0432d5d86dc
HSW93 <qazsdf963@gmail.com>
2020-12-11 06:19:10 +0900

    포토라이브러리 저장기능 캡쳐기능 구현

--- FullScreenCamera/CameraViewController.swift
+++ FullScreenCamera/CameraViewController.swift
@@ -24,7 +24,7 @@ class CameraViewController: UIViewController {
     let photoOutput = AVCapturePhotoOutput()
     
     let sessionQueue = DispatchQueue(label: "session Queue")
-    let videoDeviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera, .builtInWideAngleCamera, .builtInTrueDepthCamera], mediaType: .video, position: .back)
+    let videoDeviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera, .builtInWideAngleCamera, .builtInTrueDepthCamera], mediaType: .video, position: .unspecified)
 
     @IBOutlet weak var photoLibraryButton: UIButton!
     @IBOutlet weak var previewView: PreviewView!
@@ -102,28 +102,65 @@ class CameraViewController: UIViewController {
                     }
                     self.captureSession.commitConfiguration()
                     
-                    
-                    
-                } catch {
+                    DispatchQueue.main.async {
+                    self.updateSwitchCameraIcon(position: preferredPosition)
+                    }
+                } catch let error {
+                    print(" error occured while creating device input: \(error.localizedDescription)")
             }
         }
+            
+            
     }
     }
     func updateSwitchCameraIcon(position: AVCaptureDevice.Position) {
         // TODO: Update ICON
-        
+        switch position {
+        case .front:
+            let image = #imageLiteral(resourceName: "ic_camera_front")
+            switchButton.setImage(image, for: .normal)
+        case .back:
+            let image = #imageLiteral(resourceName: "ic_camera_rear")
+            switchButton.setImage(image, for: .normal)
+        default:
+            break
+        }
         
     }
     
     @IBAction func capturePhoto(_ sender: UIButton) {
         // TODO: photoOutput의 capturePhoto 메소드
-
+//        orientation
+//        photooutput
+        
+        let videoPreviewLayerOrientation = self.previewView.videoPreviewLayer.connection?.videoOrientation
+        sessionQueue.async {
+            let connection = self.photoOutput.connection(with: .video)
+            connection?.videoOrientation = videoPreviewLayerOrientation!
+            let setting = AVCapturePhotoSettings()
+            self.photoOutput.capturePhoto(with: setting, delegate: self)
+        }
 
     }
     
     
     func savePhotoLibrary(image: UIImage) {
         // TODO: capture한 이미지 포토라이브러리에 저장
+        
+        PHPhotoLibrary.requestAuthorization { status in
+            if status == .authorized {
+//                저장
+                PHPhotoLibrary.shared().performChanges ({
+                    PHAssetChangeRequest.creationRequestForAsset(from: image)
+                }) { (success, error) in
+                    print(" --> 이미지 저장 완료했나? \(success)")
+                }
+            } else {
+//                다시요청
+                print("--> 권한을 아직 받지못함")
+                
+            }
+            }
     }
 }
 
@@ -152,6 +189,7 @@ extension CameraViewController {
             
             if captureSession.canAddInput(videoDeviceInput){
                 captureSession.addInput(videoDeviceInput)
+                self.videoDeviceInput = videoDeviceInput
             } else {
                 captureSession.commitConfiguration()
                 return
@@ -199,6 +237,12 @@ extension CameraViewController: AVCapturePhotoCaptureDelegate {
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
         // TODO: capturePhoto delegate method 구현
         
+        guard error == nil else { return }
+        guard let imageData = photo.fileDataRepresentation() else { return }
+        guard let image = UIImage(data: imageData) else { return }
         
+        self.savePhotoLibrary(image: image)
     }
+    
+    
 }
